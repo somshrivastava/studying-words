dd47a80293f5a55f3cce98350418d63c0e15edac
somShrivastava <som.shrivastava@gmail.com>
2020-09-27 17:33:31 -0400

    Files

---
+++ studying-words/src/environments/environment.staging.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBZMwBUHsxFlwv7ocGmmW8W-8dSWqGDlP8",
+    authDomain: "stage-b47a4.firebaseapp.com",
+    databaseURL: "https://stage-b47a4.firebaseio.com",
+    projectId: "stage-b47a4",
+    storageBucket: "stage-b47a4.appspot.com",
+    messagingSenderId: "1033830470729",
+    appId: "1:1033830470729:web:b69419b0c5613770c01b98",
+    measurementId: "G-H31Y15NN6F"
+  }
+};
